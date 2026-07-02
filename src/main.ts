84aef3e7580971120b6d8139f15cc482cf35bcb5
abiao <eabiao@163.com>
2026-07-02 19:35:44 +0800

    back

--- src/main.ts
+++ src/main.ts
@@ -12,11 +12,20 @@ async function greet() {
   }
 }
 
+async function test1() {
+  
+}
+
 window.addEventListener("DOMContentLoaded", () => {
   greetInputEl = document.querySelector("#greet-input");
   greetMsgEl = document.querySelector("#greet-msg");
   document.querySelector("#greet-form")?.addEventListener("submit", (e) => {
     e.preventDefault();
     greet();
+  });
+
+  document.getElementById('testBtn')?.addEventListener("click", (e) => {
+    e.preventDefault();
+    test1();
   });
 });
